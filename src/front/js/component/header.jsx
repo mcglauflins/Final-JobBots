064154397285d467874f968286cf45390912b3a4
mcglauflins <sarahriebschlager@gmail.com>
2022-03-30 23:43:48 +0000

    still work on button, fin title

---
+++ src/front/js/component/header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => (
+	<header className="header mt-auto py-3 text-center" id="header">
+		<div>
+      <h3 id="h3">Job Applications</h3>
+      <h1 id="h1">Simplified</h1>
+    </div>
+    <button>Start Here!</button>
+	</header>
+);
