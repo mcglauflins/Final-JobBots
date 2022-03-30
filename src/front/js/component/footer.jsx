3a13d68d9b08710253b9d0218f083e7f89266e40
mcglauflins <sarahriebschlager@gmail.com>
2022-03-30 22:45:04 +0000

    ron and sarah work on front end

--- src/front/js/component/footer.jsx
+++ src/front/js/component/footer.jsx
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
+	<footer className="footer mt-auto py-3 text-center" id="footer">
 		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
+			Made with <i className="fa fa-heart text-danger" /> by Ernesto, Sarah, Chris, & Ron
 		</p>
 	</footer>
 );
