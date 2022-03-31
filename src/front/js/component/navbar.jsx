5318ed43f810516788e4744ed6239e1850b0d41d
ron24g <ron24g@yahoo.com>
2022-03-31 01:01:35 +0000

    first commit

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -5,11 +5,12 @@ export const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light" id="navbar">
       <div className="container" >
-        <span className="navbar-brand mb-0 h1" >JobBots</span>
+        <span className="navbar-brand mb-0 h1" style={{color:'#fff'}}>JobBots</span>
+        <img src= "src/front/img/logo.png"/>
         <div className="ml-auto">
           <Link to="/demo">
             <button className="btn btn-primary">
-              Check the Context in action
+            <i className="fa-duotone fa-bars"></i>
             </button>
           </Link>
         </div>
