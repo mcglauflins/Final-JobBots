f938fba099ebaaee77fe55bf40ef286dc01427d4
ron24g <ron24g@yahoo.com>
2022-04-01 02:02:32 +0000

    second edit

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo from "/workspace/Final-JobBots/src/front/img/logo.png";
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light" id="navbar">
-      <div className="container" >
-        <span className="navbar-brand mb-0 h1" style={{color:'#fff'}}>JobBots</span>
-        <img src= "src/front/img/logo.png"/>
+      <div className="container">
+        <img src={logo} />
         <div className="ml-auto">
           <Link to="/demo">
             <button className="btn btn-primary">
-            <i className="fa-duotone fa-bars"></i>
+              <i class="fas fa-bars"></i>
             </button>
           </Link>
         </div>
