4632e4c4e986f13c198eb9bc2a80a2baf4976408
mcglauflins <sarahriebschlager@gmail.com>
2022-04-04 18:28:12 +0000

    work on bootstrap-react

--- src/front/js/component/header.jsx
+++ src/front/js/component/header.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Stack from "react-bootstrap/Stack";
 
-export const Header = () => (
-	<header className="header mt-auto py-3 text-center" id="header">
-		<div>
-      <h3 id="h3">Job Applications</h3>
-      <h1 id="h1">Simplified</h1>
-    </div>
-    <button>Start Here!</button>
-	</header>
-);
+export const Header = () => {
+  return (
+    <Container className="container">
+      <Row className="rowHead">
+        <Col>One column</Col>
+      </Row>
+      <Row>
+        <Col>One of two columns</Col>
+        <Col className="colHead">
+          <h3 className="header1">Job Applications</h3>
+        </Col>
+      </Row>
+      <Row className="rowHead1">
+        <Col>One of two columns</Col>
+        <Col className="colHead1">
+          <h1 className="header2">Simplified</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col></Col>
+        <Col></Col>
+        <Col className="colHead">
+          <button className="btn1">Start Here!</button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
