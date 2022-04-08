df2e80a0f8043c626e723c6bb60322d8563f2cc3
ron24g <ron24g@yahoo.com>
2022-04-08 23:23:10 +0000

    nav design fin

--- src/front/js/component/navbar.jsx
+++ src/front/js/component/navbar.jsx
@@ -9,73 +9,77 @@ export const Navbar = () => {
         <img src={logo} />
         <div className="ml-auto">
           <div className="icons-right">
-            <Link to="/demo">
-              <button
-                className="btn btn-primary"
-                type="button"
-                id="dropdownMenuButton1"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <i className="fas fa-bars"></i>
-              </button>
-              <ul
-                className="dropdown-menu"
-                aria-labelledby="dropdownMenuButton1"
-              >
-                <li>
-                  <a className="dropdown-item" href="#">
-                    About
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    How it works
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Download
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    FAQ
-                  </a>
-                </li>
-              </ul>
-            </Link>
-            <Link to="/demo">
-              <button
-                className="btn btn-primary"
-                type="button"
-                id="dropdownMenuButton1"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                <i className="fas fa-user"></i>
-              </button>
-              <ul
-                className="dropdown-menu"
-                aria-labelledby="dropdownMenuButton1"
-              >
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Login
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Sign Up
-                  </a>
-                </li>
-                <li>
-                  <a className="dropdown-item" href="#">
-                    Dashboard
-                  </a>
-                </li>
-              </ul>
-            </Link>
+            <div className="hamburger">
+              <Link to="/demo">
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i className="fas fa-bars"></i>
+                </button>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton1"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      About
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      How it works
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Download
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      FAQ
+                    </a>
+                  </li>
+                </ul>
+              </Link>
+            </div>
+            <div className="profile">
+              <Link to="/demo">
+                <button
+                  className="btn btn-primary"
+                  type="button"
+                  id="dropdownMenuButton1"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i className="fas fa-user"></i>
+                </button>
+                <ul
+                  className="dropdown-menu"
+                  aria-labelledby="dropdownMenuButton1"
+                >
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Login
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Sign Up
+                    </a>
+                  </li>
+                  <li>
+                    <a className="dropdown-item" href="#">
+                      Dashboard
+                    </a>
+                  </li>
+                </ul>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
