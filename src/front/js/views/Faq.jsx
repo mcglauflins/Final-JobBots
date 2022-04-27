55b7dd688219802acdb3be65d3b29dba712ffded
ron24g <ron24g@yahoo.com>
2022-04-27 22:26:48 +0000

    finish Q & A

--- src/front/js/views/Faq.jsx
+++ src/front/js/views/Faq.jsx
@@ -6,33 +6,44 @@ import "../../styles/about.css";
 function Faqs() {
   return (
     <div className="faqs">
-     <h1 className="header">FAQS</h1>
-        <Accordion className="faq-rw">
-          <Accordion.Item eventKey="0">
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="1">
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+      <h1 className="header">FAQS</h1>
+      <Accordion className="faq-rw">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>
+            Q: Can I use Jobbots with any jobsearch website?
+          </Accordion.Header>
+          <Accordion.Body>
+            A: At the moment Jobbots can only be used with , but in the short
+            future we plan on expanding Jobbots so that it is compatible with
+            most job search websites.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="1">
+          <Accordion.Header>
+            Q: Do I have to create an account to use Jobbots?
+          </Accordion.Header>
+          <Accordion.Body>
+            A: No, Jobbots works with your account that is signed into the job
+            search website.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="2">
+          <Accordion.Header>
+            Q: Will Jobbots work on my device? </Accordion.Header>
+          <Accordion.Body>
+            A: Yes, Jobbots will work on most devices.
+          </Accordion.Body>
+        </Accordion.Item>
+        <Accordion.Item eventKey="3">
+          <Accordion.Header>
+            Q: Do I have to create an account to use Jobbots?
+          </Accordion.Header>
+          <Accordion.Body>
+            A: No, Jobbots works with your account that is signed into the job
+            search website.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
     </div>
   );
 }
