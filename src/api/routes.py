fd4c71d1b3bc8f5b994139ed7179e4e9ae39c141
ErnestoXG <ggoku02@gmail.com>
2022-04-29 23:01:39 +0000

    Sign Up route now working properly

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,9 +1,15 @@
 """
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
+import os
 from flask import Flask, request, jsonify, url_for, Blueprint
+from flask_migrate import Migrate
+from flask_swagger import swagger
+from flask_cors import CORS
+from api.admin import setup_admin
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+
 
 # JWT
 from flask_jwt_extended import create_access_token
@@ -13,6 +19,13 @@ from flask_jwt_extended import JWTManager
 
 api = Blueprint('api', __name__)
 app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+MIGRATE = Migrate(app, db)
+db.init_app(app)
+CORS(app)
+setup_admin(app)
 
 # Setup the Flask-JWT-Extended extension
 app.config["JWT_SECRET_KEY"] = "fhL>?`[L/FNd9aN>"  # Change this!
@@ -23,28 +36,31 @@ jwt = JWTManager(app)
 # create_access_token() function is used to actually generate the JWT.
 @api.route("/signup", methods=["POST"])
 def postSignup():
-    request_body_credentials = request.get_json()
-    userCredential = User(email=request_body_credentials["email"], password=request_body_credentials["password"])
-    db.session.add(userCredential)
-    db.session.commit()
+    request_body_credentials = request.get_json(force=True)
+    emailExists = bool(User.query.filter_by(email=request_body_credentials["email"]).first())
 
-    return jsonify(request.get_json()),200
+    if emailExists:
+        return jsonify("Email is already in use.")
+    else:
+        userCredential = User(email=request_body_credentials["email"], password=request_body_credentials["password"])
+        db.session.add(userCredential)
+        db.session.commit()
 
-@api.route("/accounts", methods=["GET"])
-def getAccounts():
-    accounts = User.query.all()
-    all_accounts = list(map(lambda x: x.serialize(), accounts))
-    return jsonify(all_accounts)
+        return jsonify("Success!")
 
 
 @api.route("/login", methods=["POST"])
 def login():
-    username = request.json.get("username", None)
+    email = request.json.get("email", None)
     password = request.json.get("password", None)
-    if username != User.username or password != User.password:
+
+    emailMatches = db.session.query(db.exists().where(User.email == email)).scalar()
+    passwordMatches = db.session.query(db.exists().where(User.password == password)).scalar()
+
+    if emailMatches == False and passwordMatches == False:
         return jsonify({"msg": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity=username)
+    access_token = create_access_token(identity=email)
     return jsonify(access_token=access_token)
 
 @api.route("/dashboard", methods=["GET"])
@@ -52,6 +68,13 @@ def login():
 def dashboard():
     current_user = get_jwt_identity()
     return jsonify(logged_in_as=current_user), 200
+
+@api.route("/accounts", methods=["GET"])
+def getAccounts():
+    accounts = User.query.all()
+    all_accounts = list(map(lambda x: x.serialize(), accounts))
+    return jsonify(all_accounts)
+
 
 if __name__ == "__main__":
     app.run()
