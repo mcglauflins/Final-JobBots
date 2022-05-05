bf228dea2678a0f5552b1255a50042ba9ce06d85
ron24g <ron24g@yahoo.com>
2022-05-05 21:04:36 +0000

    commented out footer

--- src/front/js/layout.jsx
+++ src/front/js/layout.jsx
@@ -46,7 +46,7 @@ const Layout = () => {
               <h1>Not found!</h1>
             </Route>
           </Switch>
-           <Footer />
+       {  /* <Footer /> */}
         </ScrollToTop>
       </BrowserRouter>
     </div>
