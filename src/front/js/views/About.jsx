732f665bcadf1f4efe97d1004dff7403b0ab2a1e
ron24g <ron24g@yahoo.com>
2022-05-09 01:36:47 +0000

    need url's for icons

--- src/front/js/views/About.jsx
+++ src/front/js/views/About.jsx
@@ -17,19 +17,62 @@ function About() {
       <h1 className="header">The Team</h1>
       <Container className="content" fluid="md">
         <CardGroup>
+          <Card className="card">
+            <Card.Img variant="top" src={updated} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Card.Link href="#">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </Card.Link>
+              <Card.Link href="https://github.com/mcglauflins">
+                <FontAwesomeIcon icon={faGithub} />
+              </Card.Link>
+            </Card.Footer>
+          </Card>
+          <Card className="card">
+            <Card.Img variant="top" src={updated} />
+            <Card.Body>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This card has even longer content
+                than the first to show that equal height action.
+              </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+              <Card.Link href="#">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </Card.Link>
+              <Card.Link href="#">
+                <FontAwesomeIcon icon={faGithub} />
+              </Card.Link>
+            </Card.Footer>
+          </Card>
           <Card>
             <Card.Img variant="top" src={Ron} />
             <Card.Body>
               <Card.Title>Ron Butler</Card.Title>
               <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
+                Hello I'm Ron, I am a full-stack software developer from New
+                Haven, CT. My hobbies include coding, sports, working on cars,
+                and playing video games. I am a recent student of 4Geeks academy
+                based in Miami, FL., and co-contributor to this project.
               </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <FontAwesomeIcon icon={faLinkedin} />
-              <FontAwesomeIcon icon={faGithub} />
+              <Card.Link href="https://www.linkedin.com/in/ronald-butler-5249a311">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </Card.Link>
+              <Card.Link href="https://github.com/ron24g">
+                <FontAwesomeIcon icon={faGithub} />
+              </Card.Link>
             </Card.Footer>
           </Card>
           <Card className="card">
@@ -43,38 +86,12 @@ function About() {
               </Card.Text>
             </Card.Body>
             <Card.Footer>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Card Link</Card.Link>
-            </Card.Footer>
-          </Card>
-          <Card className="card">
-            <Card.Img variant="top" src={updated} />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Card Link</Card.Link>
-            </Card.Footer>
-          </Card>
-          <Card className="card">
-            <Card.Img variant="top" src={updated} />
-            <Card.Body>
-              <Card.Title>Card title</Card.Title>
-              <Card.Text>
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This card has even longer content
-                than the first to show that equal height action.
-              </Card.Text>
-            </Card.Body>
-            <Card.Footer>
-              <Card.Link href="#">Card Link</Card.Link>
-              <Card.Link href="#">Card Link</Card.Link>
+              <Card.Link href="#">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </Card.Link>
+              <Card.Link href="#">
+                <FontAwesomeIcon icon={faGithub} />
+              </Card.Link>
             </Card.Footer>
           </Card>
         </CardGroup>
--- src/front/js/views/Faq.jsx
+++ src/front/js/views/Faq.jsx
@@ -13,7 +13,7 @@ function Faqs() {
             Q: Can I use SERCBot with any jobsearch website?
           </Accordion.Header>
           <Accordion.Body>
-            A: At the moment JobBots can only be used with Linkedin, but in the short
+            A: At the moment SERCbot can only be used with Linkedin, but in the short
             future we plan on expanding JobBots so that it is compatible with
             most job search websites.
           </Accordion.Body>
@@ -30,28 +30,30 @@ function Faqs() {
           <Accordion.Header>
             Q: Will SERCBot work on my device? </Accordion.Header>
           <Accordion.Body>
-            A: Currently SERCBot .
+            A: Currently SERCBot is running on PC and andriod devices.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="3">
           <Accordion.Header>
             Q: can i upload my resume on JobBots? </Accordion.Header>
           <Accordion.Body>
-            A: .
+            A: Yes, in order to use SERCbot you will need to upload your resume in .pdf format.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="4">
           <Accordion.Header>
             Q: Will JobBots apply to jobs for me? </Accordion.Header>
           <Accordion.Body>
-            A: .
+            A: Yes, SERCbot will automatically search and apply to as many jobs as your keywords indicate.
           </Accordion.Body>
         </Accordion.Item>
         <Accordion.Item eventKey="5">
           <Accordion.Header>
             Q: Will JobBots keep track of the jobs i've applied to? </Accordion.Header>
           <Accordion.Body>
-            A: .
+            A: No, SERCbot will not keep track of the jobs that you have applied to, but 
+            Linkedin will keep track of the jobs you have applied to. In future versions
+            this is a feature we plan on adding.
           </Accordion.Body>
         </Accordion.Item>
       </Accordion>
