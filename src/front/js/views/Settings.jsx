c32c6a6b94abb74ecc012120373b863aeb3136a5
ErnestoXG <ggoku02@gmail.com>
2022-05-12 04:32:21 +0000

    updated unauthorized view

--- src/front/js/views/Settings.jsx
+++ src/front/js/views/Settings.jsx
@@ -1,24 +1,34 @@
-import Sidebar from "../component/sideBar.jsx"
-import useStore from "../store/zustand"
-import {useState} from "react"
-import {LoginModal} from "../component/loginModal.jsx"
+import Sidebar from "../component/sideBar.jsx";
+import useStore from "../store/zustand";
+import { useState } from "react";
+import { LoginModal } from "../component/loginModal.jsx";
 
 const Settings = () => {
-    const [showModal, setShowModal] = useState(false);
-    const handleShowModal = () => {
-      setShowModal(!showModal);
-    };
-    const store = useStore()
+  const [showModal, setShowModal] = useState(false);
+  const handleShowModal = () => {
+    setShowModal(!showModal);
+  };
+  const store = useStore();
 
-    return store.loggedIn ? (<div className="d-flex" style={{height: "100vh" }}>
-        <Sidebar navKey="4"/>
-        <h1>Settings Page Here</h1>
-    </div>) : ( <div>
-      <h1>UNAUTHORIZED 401</h1>
-      <h4>You must <a href="#" className="link-light"onClick={()=>handleShowModal()}>login.</a> </h4>
+  return store.loggedIn ? (
+    <div className="d-flex" style={{ height: "100vh" }}>
+      <Sidebar navKey="4" />
+      <h1>Settings Page Here</h1>
+    </div>
+  ) : (
+    <div>
+      <h1>UNAUTHORIZED 404</h1>
+      <h4>
+        Please{" "}
+        <a href="#" onClick={() => handleShowModal()}>
+          login
+        </a>{" "}
+        to access dashboard.
+      </h4>
 
       <LoginModal show={showModal} onHide={handleShowModal} />
-    </div> )
-}
+    </div>
+  );
+};
 
-export default Settings
+export default Settings;
